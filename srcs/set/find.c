8bac806d239a1a9beb55974e76b5f6ad1718272e
Kim Hyewon <79592604+hyew-kim@users.noreply.github.com>
2022-05-26 17:53:41 +0900

    Fix: #2 find func does not work issue

--- srcs/set/find.c
+++ srcs/set/find.c
@@ -1,11 +1,11 @@
 #include "includes/set.h"
+#include <string.h>
 
 int bst_find(t_node* root, char *target) 
 {
-    printf("root: %s\n", root->value);
     if (root == NULL)
         return (0);
-    if (root->value == target)
+    if (strcmp(root->value, target))
         return (1);
     else if (root->value > target)
         return bst_find(root->left, target);
